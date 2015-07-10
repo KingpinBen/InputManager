aa9e6d62717e59a6ddc3d9b6747ba4c241e3c89a
Ben Griffiths <scare_tfc@hotmail.com>
2015-07-10 03:20:13 +0100

    Update KeyUsage.cs
    Added a comparer as memory is allocated when using an Enum as a key in mono when using the Default Comparer..

--- KeyUsage.cs
+++ KeyUsage.cs
@@ -1,3 +1,24 @@
+using System.Collections.Generic;
+
+/// <summary>
+/// A basic comparer to avoid Dictionary allocation during indexing.
+/// 
+/// Mono doesn't like having enum's as the key in a dictionary and allocates
+/// memory that then needs to be cleared by the GC (damned DefaultComparer)
+/// </summary>
+public sealed class KeyUsageComparer : IEqualityComparer<KeyUsage>
+{
+    public bool Equals(KeyUsage a, KeyUsage b)
+    {
+        return a == b;
+    }
+
+    public int GetHashCode(KeyUsage obj)
+    {
+        return (int)obj;
+    }
+}
+
 public enum KeyUsage
 {
     Ability1,
